94ffc687db1acc7afc0fa72f79d863d5f23c6f26
Yousef27h <yousef_salem97@yahoo.com>
2021-03-04 16:33:57 +0200

    new page

---
+++ page.js
@@ -0,0 +1,18 @@
+alert("Welcome Master")
+
+var userPreference;
+
+if (confirm("Do you want to save changes?") == true) {
+    userPreference = "Data saved successfully!";
+} else {
+    userPreference = "Save Cancelled!";
+}
+
+var person = prompt("Please enter your name");
+
+if (person != null ) {
+    document.write("Hello "+person)
+}else {
+    document.write("Liar!")
+}
+
